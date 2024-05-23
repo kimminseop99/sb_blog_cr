45932ea9229418de844bf8524f5e5ee63d2306a3
kimminseop99 <minseopkim99@gmail.com>
2024-05-23 16:44:28 +0900

    게시글 리스트 데이터 출력

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -2,12 +2,18 @@ package com.example.blog.domain.post.entity;
 
 import com.example.blog.global.jpa.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Post extends BaseEntity {
 
     private String title;
